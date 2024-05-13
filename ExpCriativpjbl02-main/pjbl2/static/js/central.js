09b9ef33db8e4c8ffab5d752941a09d9b02f7469
Navarro45 <phvnavarro@gmail.com>
2024-05-13 08:56:42 -0300

    Update 19

--- ExpCriativpjbl02-main/pjbl2/static/js/central.js
+++
@@ -1,8 +0,0 @@
- // Fetch sensor data from Flask server
-fetch('/sensor_data')
-.then(response => response.json())
-.then(data => {
-    document.getElementById('temperature').innerHTML = data.temperature + '&deg;C';
-    document.getElementById('humidity').innerHTML = data.humidity + '%';
-})
-.catch(error => console.error('Error:', error));
