fe312b67311294e5565a794c6f54902725459299
Navarro45 <phvnavarro@gmail.com>
2024-05-13 01:06:46 -0300

    Update 10

--- ExpCriativpjbl02-main/pjbl2/login.py
+++ ExpCriativpjbl02-main/pjbl2/login.py
@@ -2,7 +2,8 @@ from flask import Blueprint, request, render_template, redirect, url_for
 
 login = Blueprint("login",__name__, template_folder="templates")
 
-users = []
+user_name = []
+user_password = []
 
 
 @login.route('/register_user')
@@ -15,10 +16,14 @@ def add_user():
     if request.method == 'POST':
         user = request.form['user']
         password = request.form['password']
+        user_name.append(user)
+        user_password.append(password)
     else:
         user = request.args.get('user', None)
         password = request.args.get('password', None)
-        users[user] = password
+        user_name.append(user)
+        user_password.append(password)
+        
     return render_template("users.html", devices=users)
 
 @login.route('/remove_user')
@@ -31,7 +36,16 @@ def del_user():
     if request.method == 'POST':
         print(request.form)
         user = request.form['user']
+        for i in user_name:
+            if user == user_name[i]:
+                user_name.pop(i)
+                user_password.pop(i)
+            i += 1
     else:
         user = request.args.get('user', None)
-        users.pop(user)
+        for i in user_name:
+            if user == user_name[i]:
+                user_name.pop(i)
+                user_password.pop(i)
+            i += 1
     return render_template("users.html", devices=users)
